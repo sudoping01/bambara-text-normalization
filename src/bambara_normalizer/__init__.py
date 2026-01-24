aee2c42b944e551f963540eee3d876b5560b0c90
sudoping01 <sudoping01@gmail.com>
2026-01-24 21:19:58 +0000

    update version

--- src/bambara_normalizer/__init__.py
+++ src/bambara_normalizer/__init__.py
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "sudoping01"
 __license__ = "MIT"
 
